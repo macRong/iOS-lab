0960e2ec0aa1503080c5f63a044cfb6993436966
macRong <121071838@qq.com>
2018-05-15 10:27:19 +0800

    Add(这才是真正的第二次修改message)

---
+++ iOS-Sample/CPP/CopyClassCpp.cpp
@@ -0,0 +1,9 @@
+//
+//  CopyClassCpp.cpp
+//  iOS-Sample
+//
+//  Created by macRong on 2018/5/11.
+//  Copyright © 2018年 macRong. All rights reserved.
+//
+
+#include "CopyClassCpp.hpp"
---
+++ iOS-Sample/CPP/CopyClassCpp.hpp
@@ -0,0 +1,16 @@
+//
+//  CopyClassCpp.hpp
+//  iOS-Sample
+//
+//  Created by macRong on 2018/5/11.
+//  Copyright © 2018年 macRong. All rights reserved.
+//
+
+
+/** copy构造函数 */
+#ifndef CopyClassCpp_hpp
+#define CopyClassCpp_hpp
+
+#include <stdio.h>
+
+#endif /* CopyClassCpp_hpp */
